ff08b41768a21b3f4a7a1467dd2183b12130cd30
Matteo Nastasi <nastasi@openquake.org>
2018-09-21 09:53:12 +0200

    fix wrong delta sign and refactored a variables names

--- txt_raster_importer.py
+++ txt_raster_importer.py
@@ -20,33 +20,32 @@ def main():
     site_csv.writerow(["site_id", "lon", "lat"])
 
     raster = gdal.Open(fname)
-    print(raster)
 
-    band = raster.GetRasterBand(1)
-    print(band)
+    # band = raster.GetRasterBand(1)
+
     rasterArray = raster.ReadAsArray()
 
     # xsize = raster.RasterXSize
     # ysize = raster.RasterYSize
 
-    x, delta, _, y, _, _ = raster.GetGeoTransform()
-    if x > 180.0:
-        x = x - 360.0
+    lon, lon_delta, _, lat, _, lat_delta = raster.GetGeoTransform()
+    if lon > 180.0:
+        lon = lon - 360.0
 
     rizi = 0
     eid = 0
 
     sid = 0
 
-    lat_out = y + (delta / 2.0)
+    lat_out = lat + (lat_delta / 2.0)
     for row in rasterArray:
-        lon_out = x + (delta / 2.0)
+        lon_out = lon + (lon_delta / 2.0)
         for el in row:
             site_csv.writerow([sid, "%.5f" % lon_out, "%.5f" % lat_out])
             gmf_csv.writerow([rizi, sid, eid, el])
-            lon_out += delta
+            lon_out += lon_delta
             sid += 1
-        lat_out += delta
+        lat_out += lat_delta
 
 
 if __name__ == "__main__":
